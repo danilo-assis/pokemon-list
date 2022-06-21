705144d2f42efbb0de8373978b9f1fe44f028b30
danilo-assis <daniloassis_vg@hotmail.com>
2022-06-21 14:31:26 +0100

    Add: NavBar Component

--- src/App.js
+++ src/App.js
@@ -12,19 +12,23 @@ import Container from "@mui/material/Container";
 import Main from "./Pages/Main/Main";
 import Detail from "./Pages/Detail/Detail";
 import Favorite from "./Pages/Favorite/Favorite";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <StyledContainer maxWidth="false" disableGutters="true">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/detail" element={<Detail />} />
-          <Route path="/favorite" element={<Favorite />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
-    </StyledContainer>
+    <>
+      <Navbar />
+      <StyledContainer maxWidth="false" disableGutters="true">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/favorite" element={<Favorite />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      </StyledContainer>
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Zoom from '@mui/material/Zoom';
+
+import Logo from '../../Assets/img/pokemonLogo.png';
+
+const handleSrollTopClick = (event) => {
+  const anchor = (event.target.ownerDocument || document).querySelector(
+    '#back-to-top-anchor',
+  );
+
+  if (anchor) {
+    anchor.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    });
+  }
+};
+
+function ScrollTop({ children }) {
+  const trigger = useScrollTrigger();
+
+  return (
+    <Zoom in={trigger}>
+      <StyledBox
+        onClick={handleSrollTopClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </StyledBox>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default function Navbar(props) {
+  return (
+    <>
+      <CssBaseline />
+
+      <StyledAppBar>
+        <LogoContainer onClick={handleSrollTopClick}>
+          <LogoImg src={Logo} alt="Pokémon" />
+        </LogoContainer>
+      </StyledAppBar>
+
+      <Toolbar id="back-to-top-anchor" />
+
+      <ScrollTop {...props}>
+        <Fab size="small" aria-label="go to the top of the page">
+          <StyledKeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </>
+  );
+}
+
+const StyledBox = styled(Box)`
+  z-index: 9;
+
+  & button {
+    background-color: var(--primary-color);
+
+    &:hover {
+      background-color: var(--primary-color);
+    }
+  }
+`;
+
+const StyledKeyboardArrowUpIcon = styled(KeyboardArrowUpIcon)`
+  color: var(--secondary-color);
+`;
+
+const StyledAppBar = styled(AppBar)`
+  &.MuiPaper-root {
+    background-color: var(--primary-color);
+  }
+`;
+
+const LogoContainer = styled.div`
+  margin: 0 auto;
+  width: 72px;
+  height: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const LogoImg = styled.img`
+  height: 64px;
+  width: 64px;
+`;
