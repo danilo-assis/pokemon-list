09d4ba581111f66a2c47a31fe3602269889a9aaf
danilo-assis <daniloassis_vg@hotmail.com>
2022-06-23 14:49:07 +0100

    Add: PokemonCards on Main

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPokemonList } from '../../redux/modules/PokemonList';
 import { Link } from 'react-router-dom';
 
+import PokemonCard from '../../components/PokemonCard/PokemonCard';
+import slogan from '../../Assets/img/gottaCatchThemAll.png';
 function Main() {
   const [currentPage, setCurrentPage] = useState(1);
   const dispatch = useDispatch();
@@ -15,24 +19,44 @@ function Main() {
   }, []);
   return (
     <>
-      {pokemonPageList.map((pokemon) => (
-        <Link
-          to={{
-            pathname: '/detail',
-            search: `?id=${pokemon.id}`,
-          }}
-          key={pokemon.id}
-        >
-          <div key={pokemon.id}>{pokemon.name}</div>
-
-          <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-            alt=""
-          />
-        </Link>
-      ))}
+      <Slogan>
+        <img src={slogan} alt="Pokemon slogan" />
+      </Slogan>
+      <CardsContainer>
+        {pokemonPageList.map((pokemon) => (
+          <StyledLink
+            to={{
+              pathname: '/detail',
+              search: `?id=${pokemon.id}`,
+            }}
+            key={pokemon.id}
+          >
+            <PokemonCard name={pokemon.name} id={pokemon.id} />
+          </StyledLink>
+        ))}
+      </CardsContainer>
     </>
   );
 }
+
+const StyledLink = styled(Link)`
+  margin-top: 80px;
+`;
+
+const CardsContainer = styled.div`
+  margin: 0 auto;
+  max-width: 1300px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+const Slogan = styled.div`
+  padding-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Main;
---
+++ src/components/PokemonCard/PokemonCard.js
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+export default function PokemonCard({ name, id }) {
+  return (
+    <StyledCard
+      sx={{
+        bgcolor: 'var(--primary-color)',
+        borderRadius: 5,
+        border: '3px solid #000',
+      }}
+    >
+      <ClickToDetail>Click to more Datails</ClickToDetail>
+      <Box>
+        <CardContent>
+          <Typography
+            component="div"
+            variant="h5"
+            sx={{
+              textTransform: 'capitalize',
+              fontSize: 20,
+              fontFamily: 'Roboto',
+              color: '#fff',
+              letterSpacing: 1.5,
+              fontWeight: 'bold',
+            }}
+          >
+            {name}
+          </Typography>
+        </CardContent>
+      </Box>
+
+      <CardMedia
+        component="img"
+        sx={{
+          width: 200,
+          height: 185,
+          bgcolor: '#fff',
+          border: '1px solid #000 ',
+        }}
+        image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        alt="Pokemon image"
+      />
+    </StyledCard>
+  );
+}
+
+PokemonCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+const StyledCard = styled(Card)`
+  margin: 0 40px;
+  width: 200px;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  flex-direction: column-reverse;
+`;
+
+const ClickToDetail = styled.p`
+  padding: 8px;
+  width: 100%;
+  color: #000;
+  font-weight: bold;
+  text-align: center;
+  background-color: var(--secondary-color);
+  border: 0.5px solid #000;
+  /* border-radius: 5px; */
+`;
